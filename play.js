d97857db7cfc793ea2bd649605ccc96406172f74
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-10-23 21:29:05 +0000

    Update play.js

--- play.js
+++ play.js
@@ -7,4 +7,17 @@ const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
+const handleUserInput = function (i) {
+  if ( i === '\u0003') {
+    return;
+  }
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserinput()
+  return stdin;
+}
 
